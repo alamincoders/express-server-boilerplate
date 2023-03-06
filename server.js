070ed52435b73426c2a8b32aa7521d67713be364
alamincoders <developeralamin03@gmail.com>
2023-03-07 00:09:42 +0600

    updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/v1/user.route");
+const dbConnect = require("./utils/dbConnect");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/api/v1/users", userRouter);
 
 // db connection
+dbConnect();
 
 // listen
 
