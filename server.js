ce3a7303d8ab1dd678c73e5c94f70b47181f9841
alamincoders <developeralamin03@gmail.com>
2023-03-07 00:18:14 +0600

    express server

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/v1/user.route");
 const dbConnect = require("./utils/dbConnect");
+const errorHandler = require("./middleware/errorHandler");
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// middleware
 
 // routes
 app.use("/api/v1/users", userRouter);
@@ -15,14 +18,13 @@ app.use("/api/v1/users", userRouter);
 dbConnect();
 
 // listen
-
 app.get("/", (req, res) => {
   res.send("Welcome to Express Server");
 });
 
-/* app.all("*", () => {
-  console.log("Routes Not Found!");
-}); */
+app.all("*", (req, res) => {
+  res.send("Routes Not Found!");
+});
 
 app.listen(port, () => {
   console.log("listening on port " + port);
